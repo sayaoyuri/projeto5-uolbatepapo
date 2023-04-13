8ef16ae798cef1ae0e1aa5aa7a8371aa3498b62e
sayaoyuri <sayaoyuri.dev@gmail.com>
2023-04-12 22:35:41 -0300

    Send message feature

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,12 @@
 axios.defaults.headers.common['Authorization'] = 'M813n9erPvENXeuGPzKDL1Iu';
 let connectionInterval;
+let userName;
 
 function connectionStatus (name) {
   // console.log(connectionInterval);
   axios.post('https://mock-api.driven.com.br/api/vm/uol/status', name)
     .then((response) => {
-      console.log('Response status: - ' + response.data);
+      // console.log('Response status: - ' + response.data);
     }).catch((error) => {
       if(error.response.status === 400) {
         console.log('Erro status: - ' + error);
@@ -36,7 +37,7 @@ function getUserName (invalidUser) {
 
   invalidUser === true ? promptMessage = 'O nome informado já está em uso, informe outro nome!' : promptMessage = 'Insira o seu nome';
 
-  const userName = prompt(promptMessage);
+  userName = prompt(promptMessage);
 
   const name = {
     name: userName
@@ -48,7 +49,28 @@ function getUserName (invalidUser) {
 getUserName();
 
 function sendMessage () {
-  // to implement
+  const content = document.getElementById('messageInput');
+  const to = 'Todos';
+  const type ='message';
+
+  const message = {
+    from: userName,
+    to: to,
+    text: content.value,
+    type: type
+  }
+
+  // console.log(message);
+  
+  axios.post('https://mock-api.driven.com.br/api/vm/uol/messages', message)
+    .then((response) => {
+      if(response.status === 200) {
+        console.log(response);
+
+        text.value = '';
+        getMessages();
+      }
+    });
 }
 
 function getMessages () {
